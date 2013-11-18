fd205da62a476a3d03fde5ce173f4e8d33cced40
Peter <cpeteryu@gmail.com>
2013-11-18 15:23:38 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
   
   resources :users
+  resources :sessions, only: [:new, :create, :destroy ]
   
   #get "users/new"
   #get "static_pages/home"
@@ -14,6 +15,11 @@ SampleApp::Application.routes.draw do
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
+
+  match '/sessions', to: 'sessions#new', via: 'get'
   
   
   # The priority is based upon order of creation: first created -> highest priority.
